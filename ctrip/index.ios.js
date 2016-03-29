cf0f7189b23df721595d037600bec4b55f66aefd
Go7hic <gtfx0209@qq.com>
2016-03-29 18:56:41 +0800

    update

---
+++ ctrip/index.ios.js
@@ -0,0 +1,29 @@
+import Index from 'Index'
+import {
+    NavigatorIOS,
+	  AppRegistry,
+    StyleSheet,
+} from 'react-native';
+
+const NV = React.createClass({
+    render: function(){
+      return(
+          <NavigatorIOS
+            style={styles.container}
+            initialRoute={{
+              title: '首页',
+              component: Index,
+            }}
+            />
+      );
+    }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
+
+
+AppRegistry.registerComponent('HelloWorld', () => NV);
